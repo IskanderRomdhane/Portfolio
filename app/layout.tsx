c1bdaa7a122f226b53f15d04d1dde4a554aa5a0e
Iskander Romdhane <skander20034@gmail.com>
2025-10-29 18:38:05 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/963312-200.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
